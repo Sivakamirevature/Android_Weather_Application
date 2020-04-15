e0a61e0e2ef6d13e81e984e884b8b73032812a77
Sivakamirevature <60508731+Sivakamirevature@users.noreply.github.com>
2020-04-15 21:31:30 +0530

    updated

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -19,77 +25,27 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 public class MainActivity extends AppCompatActivity {
+    ListView citiesList;
+    String countryList[] = {"Madurai", "Chennai", "Mumbai", "Bangalore", "Dubai", "London"};
 
-    TextView cityName, result;
-
-    public void search(View view) {
-        Button searchButton;
-        cityName = findViewById(R.id.cityName);
-        searchButton = findViewById(R.id.searchButton);
-        result = findViewById(R.id.result);
-        String city = cityName.getText().toString();
-        String content;
-        Weather1 weather = new Weather1();
-        try {
-            content = weather.execute("https://openweathermap.org/data/2.5/weather?q=" + city + "&appid=439d4b804bc8187953eb36d2a8c26a02").get();
-            Log.i("Content DATA :", content);
-            JSONObject jsonObject = new JSONObject(content);
-            String weatherData = jsonObject.getString("weather");
-            String mainTemprature = jsonObject.getString("main");
-            double visibility;
-            Log.i("Weather Data", weatherData);
-            JSONArray array = new JSONArray(weatherData);
-            String main = "", description= "", temprature = "";
-            for(int i = 0; i<array.length();i++){
-                JSONObject weatherPart = array.getJSONObject(i);
-                main = weatherPart.getString("main");
-                description = weatherPart.getString("description");
-            }
-            JSONObject mainPart = new JSONObject(mainTemprature);
-            temprature = mainPart.getString("temp");
-            visibility = Double.parseDouble(jsonObject.getString("visibility"));
-            Log.i("main",main);
-            Log.i("description",description);
-            result.setText("Main: "+main+
-                    "\nDescription: "+description +
-                    "\nTemprature: "+temprature+
-                    "\nVisibility: "+visibility/1000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public class Weather1 extends AsyncTask<String, Void, String>{
-
-        @Override
-        protected String doInBackground(String... address) {
-            try {
-                URL url = new URL(address[0]);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-                InputStream is = connection.getInputStream();
-                InputStreamReader isr = new InputStreamReader(is);
-                int data = isr.read();
-                String content = "";
-                char ch;
-                while(data != -1){
-                    ch = (char) data;
-                    content = content + ch;
-                    data  = isr.read();
-                }
-                return content;
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        citiesList = (ListView)findViewById(R.id.cityListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, countryList);
+        citiesList.setAdapter(arrayAdapter);
+        citiesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, WeatherInformation.class);
+                String message = "abc";
+                //intent.putExtra("hai", message);
+                startActivity(intent);
+            }
+        });
     }
 }
